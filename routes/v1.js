447fc73da16e9a510a078b47a2a098fe617f7845
Talouchy <danyal.lakzian@gmail.com>
2018-09-05 20:21:04 +1000

    User CRUD added.

--- routes/v1.js
+++ routes/v1.js
@@ -4,31 +4,43 @@ const User = require('../models/user');
 const router = express.Router();
 
 // get a list of Users from the db
-router.get('/ninjas', function(req, res, next){
+router.get('/user', function(req, res, next){
     User.find({}).then((user) => {
         res.send(user);
-    }).catch(next);
+    }).catch((err) => next(createError(422, err.message)));
 });
 
 // add a new User to the db
-router.post('/ninjas', function(req, res, next) {
+router.post('/user', function(req, res, next) {
     User.create(req.body)
     .then((user) => {
         res.send(user);
     })
-    .catch((err) => {
-        next(createError(422, err.message))
+    .catch((err) => next(createError(422, err.message)));
+});
+
+// update a User in the db
+router.put('/user/:id', function(req, res, next){
+    User.findOneAndUpdate({_id: req.params.id}, req.body)
+    .then((user) => {
+        User.findOne({_id: req.params.id})
+        .then((user) => {
+            res.send(user);
+        })
+        .catch((err) => next(createError(422, err.message)));
     })
+    .catch((err) => next(createError(422, err.message)));
 });
 
-// update a ninja in the db
-router.put('/ninjas/:id', function(req, res, next){
-    res.send({type: 'PUT'});
-});
-
-// delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res, next){
-    res.send({type: 'DELETE'});
+// delete a User from the db
+router.delete('/user/:id', function(req, res, next){
+    User.deleteOne({_id: req.params.id})
+    .then((user) => {
+        if (user.ok === true, user.n == 0)
+            throw createError(404, "No Such User");
+        res.send(user);
+    })
+    .catch((err) => next(createError(422, err.message)));
 });
 
 module.exports = router;
